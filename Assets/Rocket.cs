8bd4cd36081005575a1244ad721ebf0adef3c996
stiwari64 <stiwari64@gatech.edu>
2020-12-13 21:28:42 -0600

    feat: Add booster sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,14 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
        if (Input.GetKey(KeyCode.A))
         {
